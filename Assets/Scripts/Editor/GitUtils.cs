70dbc25022c8e0284022b58fd284a7ffe7ef9c44
mnicolas94 <miguelcok27@gmail.com>
2022-06-13 15:42:26 -0400

    FEAT: add git utils to pull changes

--- Assets/Scripts/Editor/GitUtils.cs
+++ Assets/Scripts/Editor/GitUtils.cs
@@ -1,7 +1,14 @@
-﻿namespace Editor
+﻿using UnityEditor;
+
+namespace Editor
 {
     public static class GitUtils
     {
-        
+        [MenuItem("TSM/Version control/Pull")]
+        public static void Pull()
+        {
+            Utils.Editor.GitUtils.Restore();
+            Utils.Editor.GitUtils.Pull();
+        }
     }
 }
