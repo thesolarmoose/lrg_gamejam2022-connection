60e7edaa76cb02419a802565c774d0249550cac6
mnicolas94 <miguelcok27@gmail.com>
2022-06-14 10:21:18 -0400

    FEAT: editor menu item to remove hook objects from scene

---
+++ Assets/Scripts/Editor/HooksUtils.cs
@@ -0,0 +1,32 @@
+﻿using Hook;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class HooksUtils
+    {
+        [MenuItem("Connection/Remove hooks")]
+        public static void RemoveHooks()
+        {
+            var hooks = Object.FindObjectsOfType<RetractableHook>();
+            foreach (var hook in hooks)
+            {
+                var startTip = hook.Rope.StartPoint;
+                var endTip = hook.Rope.EndPoint;
+                if (Application.isPlaying)
+                {
+                    Object.Destroy(hook.gameObject);
+                    Object.Destroy(startTip.gameObject);
+                    Object.Destroy(endTip.gameObject);
+                }
+                else
+                {
+                    Object.DestroyImmediate(hook.gameObject);
+                    Object.DestroyImmediate(startTip.gameObject);
+                    Object.DestroyImmediate(endTip.gameObject);
+                }
+            }
+        }
+    }
+}
