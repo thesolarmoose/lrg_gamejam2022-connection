17da5447c8c4e1208c46b5e2a2c37b1fcb854b6b
mnicolas94 <miguelcok27@gmail.com>
2022-06-17 17:24:44 -0400

    FEAT: add retracting rope sound

--- Assets/Scripts/Audio/AudioReferenceSource.cs
+++ Assets/Scripts/Audio/AudioReferenceSource.cs
@@ -6,7 +6,7 @@ namespace Audio
     {
         [SerializeField] private SoundReferenceEnum _reference;
         [SerializeField] private AudioSource _audioSource;
-
+        
         public bool IsPlaying => _audioSource.isPlaying;
         
         public SoundReferenceEnum Reference
@@ -20,12 +20,31 @@ namespace Audio
             var clip = AudioReferences.Instance.GetAudio(_reference);
             if (_audioSource != null)
             {
+                _audioSource.clip = clip;
+                _audioSource.Play();
+            }
+            else
+            {
+                AudioSource.PlayClipAtPoint(clip, transform.position);
+            }
+        }
+        
+        public void PlayOneShot()
+        {
+            var clip = AudioReferences.Instance.GetAudio(_reference);
+            if (_audioSource != null)
+            {
                 _audioSource.PlayOneShot(clip);
             }
             else
             {
                 AudioSource.PlayClipAtPoint(clip, transform.position);
             }
+        }
+
+        public void Stop()
+        {
+            _audioSource.Stop();
         }
     }
 }
