48657a94dbddd208e8040dbe423e3e766369e7ec
mnicolas94 <miguelcok27@gmail.com>
2022-06-18 15:10:25 -0400

    FIX: remove old pull

--- Assets/Scripts/Editor/GitUtils.cs
+++ Assets/Scripts/Editor/GitUtils.cs
@@ -4,11 +4,11 @@ namespace Editor
 {
     public static class GitUtils
     {
-        [MenuItem("TSM/Version control/old Pull")]
-        public static void Pull()
-        {
-            Utils.Editor.GitUtils.Restore();
-            Utils.Editor.GitUtils.Pull();
-        }
+//        [MenuItem("TSM/Version control/old Pull")]
+//        public static void Pull()
+//        {
+//            Utils.Editor.GitUtils.Restore();
+//            Utils.Editor.GitUtils.Pull();
+//        }
     }
 }
