f93a300ec078c37652c318dd8a16649871268f55
mnicolas94 <miguelcok27@gmail.com>
2022-06-19 01:29:20 -0400

    FEAT: level 3, enemies dont attack while being pulled by rope, credits scene
    close #25, close #15, close #10

---
+++ Assets/Scripts/UI/CreditsMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CreditsMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _quitButton;
+
+        private void Start()
+        {
+            _quitButton.onClick.AddListener(Quit);
+        }
+
+        private void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
