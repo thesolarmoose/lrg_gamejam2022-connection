e21c4562d7d373357b5299981dbb8f312a8a3929
mnicolas94 <miguelcok27@gmail.com>
2022-06-18 23:01:27 -0400

    FEAT: add player's health UI
    close #21

---
+++ Assets/Scripts/UI/CharacterHealthUi.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using Character;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CharacterHealthUi : MonoBehaviour
+    {
+        [SerializeField] private Health characterHealth;
+        
+        [Space]
+        
+        [SerializeField] private GameObject heartPrefab;
+        [SerializeField] private GameObject heartlessPrefab;
+        [SerializeField] private RectTransform heartsContainer;
+        
+        private IEnumerator Start()
+        {
+            characterHealth.eventDamaged.AddListener(_ => UpdateUi());
+            
+            yield return null;
+            
+            UpdateUi();
+        }
+
+        private void UpdateUi()
+        {
+            Clear();
+            int health = characterHealth.CurrentHealth;
+
+            for (int i = 0; i < health; i++)
+            {
+                AddHeart();
+            }
+
+            int toMax = characterHealth.MaxHealth - health;
+            for (int i = 0; i < toMax; i++)
+            {
+                AddEmptyHeart();
+            }
+        }
+
+        private void AddHeart()
+        {
+            Instantiate(heartPrefab, heartsContainer);
+        }
+        
+        private void AddEmptyHeart()
+        {
+            Instantiate(heartlessPrefab, heartsContainer);
+        }
+
+        private void Clear()
+        {
+            var children = heartsContainer.GetComponentsInChildren<Image>();
+            foreach (var item in children)
+            {
+                Destroy(item.gameObject);
+            }
+        }
+    }
+}
